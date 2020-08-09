201293f0ab423e7c902378bd6ec76c14bf0ed15e
Clay Clecidor <clay.clecidor@aexp.com>
2020-08-09 13:01:29 -0400

    Added factories and seeders for: User and Product modified:   database/factories/ProductFactory.php modified:   database/factories/UserFactory.php modified:   database/seeds/DatabaseSeeder.php modified:   database/seeds/ProductSeeder.php new file:   database/seeds/UserSeeder.php

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->text(30),
+        'description' => $faker->text(200),
+        'price' => $faker->randomFloat(2, 1, 100),
     ];
 });
